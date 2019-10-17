ec18956d642e859c808ea959a3d46d3affad049b
Artem Loginov <logart2007@gmail.com>
2019-10-17 19:36:01 +0300

    Initial commit

---
+++ src/main/java/org/logart/BootApp.java
@@ -0,0 +1,13 @@
+package org.logart;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class BootApp {
+
+    public static void main(String[] args) {
+        run(BootApp.class, args);
+    }
+}
